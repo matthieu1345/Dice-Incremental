789e716fe2559d556d5f66be1fa9b893209ba19e
matthieu1345-Desktop <matthieuberg@hotmail.com>
2018-02-20 00:06:38 +0100

    added dice
    added first part of dice

---
+++ Dice Incremental/Assets/Scripts/DiceStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceStats : MonoBehaviour
+{
+	[SerializeField]
+	private int power = 1;
+	[SerializeField]
+	private int sides = 3;
+	private int baseSides = 3;
+	[SerializeField]
+	private int goalSides = 6;
+	[SerializeField]
+	private int magic = 0;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		sides = baseSides;
+		DiceManager.instance.AddDice(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddPower()
+	{
+		sides++;
+		power++;
+
+		if (sides <= goalSides)
+			return;
+
+		sides = baseSides;
+		goalSides++;
+		magic++;
+	}
+}
---
+++ Dice Incremental/Assets/Scripts/Managers/DiceManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour
+{
+
+	public static DiceManager instance = null;
+
+	[SerializeField]
+	private Transform dicePrefab = null;
+
+	private List<DiceStats> allDice;
+
+	public void AddDice(DiceStats dice)
+	{
+		allDice.Add(dice);
+	}
+
+	public void RollAll()
+	{
+		for (int i = 0; i < allDice.Count; i++)
+		{
+			allDice[i].AddPower();
+		}
+	}
+
+	public void CreateDice()
+	{
+		Instantiate(dicePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(this);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
