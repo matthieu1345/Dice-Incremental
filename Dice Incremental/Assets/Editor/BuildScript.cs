140c2400ffe7be68ab91a962c5ede95ab6c4045e
matthieu1345-Desktop <matthieuberg@hotmail.com>
2019-11-10 22:38:36 +0100

    + buildscript
    + buildscript v1

---
+++ Dice Incremental/Assets/Editor/BuildScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+class BuildScript
+{
+	[MenuItem( "Build/Test")]
+	static void PerformWebglBuild()
+	{
+		BuildPlayerOptions options = new BuildPlayerOptions();
+		options.scenes = FindEditorScenes();
+		options.locationPathName = "var/lib/jenkins/workspace/Dice-Incremental/docs/";
+		options.target = BuildTarget.WebGL;
+		options.options = BuildOptions.None;
+
+		BuildPipeline.BuildPlayer( options );
+	}
+
+	private static string[ ] FindEditorScenes()
+	{
+		List<string> editorScenes = new List<string>();
+		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+		{
+			if ( !scene.enabled ) continue;
+			editorScenes.Add(scene.path);
+		}
+
+		return editorScenes.ToArray();
+	}
+}
