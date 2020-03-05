b3d2bb9748f00634b64eddf9648124cb01f7b813
matthieu1345-Desktop <matthieuberg@hotmail.com>
2020-03-06 00:15:42 +0100

    added counter animation
    added animations for mana counters

--- Dice Incremental/Assets/Scripts/UI/Counter.cs
+++ Dice Incremental/Assets/Scripts/UI/Counter.cs
@@ -12,15 +12,37 @@ public class Counter : MonoBehaviour
 	private Image[] m_childSprites;
 	[ReadOnly, SerializeField]
 	private int m_counter;
+	private int m_counterCurrent = 0;
+	private int m_counterStart = 0;
+	[SerializeField]
+	private float updateLenght = 1;
+	private float updateSpeed = 0.1f;
+	private float lastUpdate = 0;
 
+	public void FixedUpdate()
+	{
+		if (m_counterCurrent == m_counter)
+			return;
 
+		lastUpdate += Time.fixedDeltaTime;
+
+		m_counterCurrent = (int)Mathf.Lerp(m_counterStart, m_counter, lastUpdate);
+
+		UpdateSprite();
+	}
 
 	public void UpdateValue(int newValue)
 	{
 		m_counter = newValue;
+		m_counterStart = m_counterCurrent;
+		lastUpdate = 0;
+	}
+
+	void UpdateSprite()
+	{
 		for ( int i = 0; i < m_childSprites.Length; i++ )
 		{
-			m_childSprites[m_childSprites.Length - i - 1].sprite = m_sprite[CalculateSpriteIndex(m_counter, i)];
+			m_childSprites[m_childSprites.Length - i - 1].sprite = m_sprite[CalculateSpriteIndex(m_counterCurrent, i)];
 		}
 	}
 
