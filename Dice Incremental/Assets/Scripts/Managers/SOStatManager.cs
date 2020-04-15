d2e1bff6ccd0c847d943567e7f4fc4e07f5be5d5
matthieu1345-Desktop <matthieuberg@hotmail.com>
2020-04-15 05:18:30 +0200

    Added SO stat manager
    Added SO stat manager
    #DIC-135

---
+++ Dice Incremental/Assets/Scripts/Managers/SOStatManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SOStatManager : InstancedMonoBehaviour<SOStatManager>
+{
+	[SerializeField]
+	private List<Basestat> m_allStatObjects = new List<Basestat>();
+
+	[SerializeField]
+	private List<StatGroup> m_allStatGroups = new List<StatGroup>();
+
+	public void ResetStats(StatTypeEnum ResetLevel)
+	{
+		foreach (Basestat stat in m_allStatObjects)
+		{
+			stat.Reset(ResetLevel);
+		}
+	}
+
+#if UNITY_EDITOR
+	[MenuItem( "Tools/Stat tools/Get all Stats")]
+	private static void GetAllStats()
+	{
+		EditorUtility.SetDirty(GetInstance());
+		GetInstance().m_allStatObjects.Clear();
+
+		Basestat[] list = Resources.LoadAll<Basestat>("_DataAssets/Stats");
+
+		foreach (Basestat stat in list)
+		{
+			GetInstance().m_allStatObjects.Add(stat);
+		}
+
+	}
+
+	[MenuItem( "Tools/Stat tools/Get all Stat groups")]
+	private static void GetAllStatGroups()
+	{
+		EditorUtility.SetDirty(GetInstance());
+		GetInstance().m_allStatGroups.Clear();
+
+		StatGroup[] list = Resources.LoadAll<StatGroup>("_DataAssets/Stats");
+
+		foreach (StatGroup group in list)
+		{
+			GetInstance().m_allStatGroups.Add(group);
+		}
+
+	}
+
+	[MenuItem("Tools/Stat tools/Get all Stat groups and singles")]
+	private static void GetAllStatsAndGroups()
+	{
+		GetAllStatGroups();
+		GetAllStats();
+	}
+#endif
+}
