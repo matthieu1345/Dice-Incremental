49cfb3ffb065794c116c5186dcd4efb324fb3c07
matthieu1345-Desktop <matthieuberg@hotmail.com>
2020-04-28 13:30:48 +0200

    make UI update on roll
    make UI update on roll
    #DIC-154

--- Dice Incremental/Assets/Scripts/UI/MenuSelector.cs
+++ Dice Incremental/Assets/Scripts/UI/MenuSelector.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class MenuSelector : InstancedMonoBehaviour<MenuSelector>
 {
@@ -24,25 +25,45 @@ public class MenuSelector : InstancedMonoBehaviour<MenuSelector>
 	private DiceMenu m_diceMenu;
 
 	[SerializeField]
-	private Dictionary<GameObject, GameObject> m_buttonMenuPair = new Dictionary<GameObject, GameObject>();
+	private Dictionary<GameObject, MenuPair> m_buttonMenuPair = new Dictionary<GameObject, MenuPair>();
 
-	private GameObject m_currentActive;
+	private MenuPair m_currentActive;
 
 
 	public void OpenMenu( GameObject button )
 	{
 		m_diceMenu.gameObject.SetActive(false);
 		if (m_currentActive != null)
-			m_currentActive.SetActive(false);
-		m_buttonMenuPair[button].SetActive(true);
+			m_currentActive.m_menu.SetActive(false);
+		m_buttonMenuPair[button].m_menu.SetActive(true);
 		m_currentActive = m_buttonMenuPair[button];
+	}
+
+	void UpdateMenu()
+	{
+		if (m_currentActive == null)
+			return;
+		
+		m_currentActive.m_button.GetComponent<Button>().onClick.Invoke();
+	}
+
+	IEnumerator UpdateMenuNextTurn()
+	{
+		yield return null;
+
+		UpdateMenu();
+	}
+
+	public void QueueMenuUpdate()
+	{
+		StartCoroutine(UpdateMenuNextTurn());
 	}
 
 	public void OpenDiceMenu( Dice dice )
 	{
 		if ( m_currentActive != null )
 		{
-			m_currentActive.SetActive(false);
+			m_currentActive.m_menu.SetActive(false);
 			m_currentActive = null;
 		}
 
@@ -55,7 +76,7 @@ public class MenuSelector : InstancedMonoBehaviour<MenuSelector>
 		base.Awake();
 		foreach ( MenuPair menuPair in m_menuPairs )
 		{
-			m_buttonMenuPair.Add(menuPair.m_button, menuPair.m_menu);
+			m_buttonMenuPair.Add(menuPair.m_button, menuPair);
 			menuPair.m_menu.SetActive(false);
 		}
 	}
